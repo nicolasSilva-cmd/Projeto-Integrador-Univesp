916870a769871c5dbe6fa2482998e107b66e61f5
Nicolas S <nicolas_derp22@outlook.com>
2024-03-25 11:04:23 -0300

    Adição do arquivo SQL

---
+++ criação_bd_estoque.sql
@@ -0,0 +1,9 @@
+create database estoque;
+
+create table produtos(
+	id int PRIMARY KEY auto_increment,
+    nome varchar(255),
+    quantidade int,
+    preco_unitario decimal(5,2),
+    preco_total decimal(5,2)
+);
